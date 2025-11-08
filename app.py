f93ef47ca61ed7cc42d74dbe6c91f3f2ec2539d7
ItzChaBoiGhoza <cguy925@gmail.com>
2025-11-07 19:25:43 -0700

    initial set up for creating a web app

---
+++ app.py
@@ -0,0 +1,36 @@
+# Importing Libaries
+import dash
+import dash_bootstrap_components as dbc
+from dash import dcc, Input, Output, html
+import plotly.express as px
+
+# Creating a Web App
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
